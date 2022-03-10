7088717314032b0559c465824cbd8d44cbe2c575
Bidipto <bidipto.7.kmr@gmail.com>
2022-03-10 23:36:01 +0530

    Delete 01_ReverseTheArray.py

--- 450QuestionSheet/01_ReverseTheArray.py
+++
@@ -1,8 +0,0 @@
-#simple solution using a reverse for loop
-def reverseWord(s):
-    res=[]
-    for i in range(len(s)-1,-1,-1):
-        res.append(s[i])
-    return "".join(res)
-    
-#inplace solution using a reverse function could be used as-well
