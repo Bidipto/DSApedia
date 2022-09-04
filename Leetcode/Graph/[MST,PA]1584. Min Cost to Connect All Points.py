968f52b10d9998d28fe1e21de5c465510c24e16a
Bidipto <bidipto.7.kmr@gmail.com>
2022-09-05 02:10:30 +0530

    5.9.22

--- Leetcode/Graph/[MST,PA]1584. Min Cost to Connect All Points.py
+++ Leetcode/Graph/[MST,PA]1584. Min Cost to Connect All Points.py
@@ -30,3 +30,70 @@ def minCostConnectPoints(self, points: List[List[int]]) -> int:
                     heapq.heappush(heap,[nxtcost, nxt])
                 
         return res
+
+# Application of kruskals algorithm
+
+
+import heapq
+class Solution:
+    #kruskal's algo with union find by rank and path compression 
+    #significantlty faster then prims algo
+    def par(self,node):
+        while node != self.parent[node]:
+            self.parent[node] = self.parent[self.parent[node]]
+            node = self.parent[node]
+        return node
+    
+    def union(self,u,v):
+        paru = self.par(u)
+        parv = self.par(v)
+        
+        #will form a cycle
+        if paru == parv:
+            return False
+        
+        if self.rank[parv]>self.rank[paru]:
+            self.parent[paru] = parv
+            self.rank[parv] += self.rank[paru]
+        else:
+            self.parent[parv] = paru
+            self.rank[paru] += self.rank[parv]
+        
+        return True 
+            
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        adj = collections.defaultdict(list)
+        
+        heap = []
+        
+        N = len(points)
+        cost = 0
+        
+        for i,(x1,y1) in enumerate(points):
+            for j,(x2,y2) in enumerate(points[i+1:],i+1):
+                heapq.heappush(heap,(dis(x1,y1,x2,y2),i,j))
+                # heapq.heappush(heap,(dis(x1,y1,x2,y2),j,i))
+                
+        # print(heap)
+            
+        self.parent = [i for i in range(N)]
+        self.rank = [1] * N
+             
+        count = 0
+        while count<N-1 and heap:
+            w,u,v = heapq.heappop(heap)
+            
+            # print(w,u,v)
+            # print(self.parent,self.rank,count)
+            if self.union(u,v) == True:
+                cost += w
+                count += 1
+                # print(count,cost)
+                # print(self.parent,self.rank,count)
+            # print()
+            
+        # print(self.parent,self.rank,count)
+        return cost 
+                    
+def dis(x1,y1,x2,y2):
+    return abs(x1-x2)+abs(y1-y2)
